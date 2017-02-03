013376e462a6de4de2efbdd8febe9f31ad8520fc
EderMarcos <skrap.marcos@gmail.com>
2017-02-03 16:40:35 -0600

    Cambios

--- config/passport/localAuth.js
+++ config/passport/localAuth.js
@@ -5,17 +5,15 @@ const User = require('../../models/modelUsers')
 
 function localConfig(app, passport, config) {
     passport.use(new LocalinStrategy({
-        usernameField    : config.localAuth.username,
-        passwordField : config.localAuth.password
+        usernameField  : config.localAuth.username,
+        passwordField  : config.localAuth.password
 
     }, (username, password, done) => {
-        process.nextTick(() => {
-            User.findOne({ username : username }, (err, user) => {
-                if (err) return done(err)
-                else if(!user) return done(null, false)
-                else if(user.password === password) return done(null, user)
-                else return done(null, false, {message: 'El usuario o contraseña con coinciden'})
-            })
+        User.findOne({ username : username }, (err, user) => {
+            if (err) return done(err)
+            else if(!user) return done(null, false)
+            else if(user.password === password) return done(null, user)
+            else return done(null, false, {message: 'El usuario o contraseña con coinciden'})
         })
     }))
     app.get('/auth/local', passport.authenticate('local', {
