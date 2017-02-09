1b0052a731b52a93bf6b9ab4ad0abb84e10f90d9
pio777 <lpab777@gmail.com>
2017-02-09 16:54:20 -0600

    Modificacin rutas .. Haber si no te afecta EDER

--- routes/routerUserPlus.js
+++ routes/routerUserPlus.js
@@ -4,10 +4,6 @@
 const express = require('express')
 const router = express.Router()
 
-<<<<<<< HEAD
-const noticiasCrudController = require('../controllers/newsCrudController')
-=======
->>>>>>> parent of 9b56467... conflict
 const viewsController = require('../controllers/viewsController')
 const newsCrudController = require('../controllers/newsCrudController')
 const demandCrudController = require('../controllers/demandCrudController')
@@ -15,43 +11,24 @@ const offerCrudController = require('../controllers/offerCrudController')
 const usersCrudController = require('../controllers/usersCrudController')
 
 // Vistas ==============================================================
-<<<<<<< HEAD
-router.get('/', usersCrudController.getViewIndexPlus)
-router.get('/ofertas/new', usersCrudController.getViewIndexPlus)
-router.get('/ofertas/:id/edit', usersCrudController.getViewIndexPlus)
+router.get('/', viewsController.getViewIndexPlus)
+router.get('/ofertas/new', viewsController.getViewIndexPlus)
+router.get('/ofertas/:id/edit', viewsController.getViewIndexPlus)
 
-router.get('/demandas/new', usersCrudController.getViewIndexPlus)
-router.get('/demandas/:id/edit', usersCrudController.getViewIndexPlus)
+router.get('/demandas/new', viewsController.getViewIndexPlus)
+router.get('/demandas/:id/edit', viewsController.getViewIndexPlus)
 
-router.get('/users/new', usersCrudController.getViewIndexPlus)
-router.get('/users/:id/edit', usersCrudController.getViewIndexPlus)
+router.get('/users/new', viewsController.getViewIndexPlus)
+router.get('/users/:id/edit', viewsController.getViewIndexPlus)
 
 
-=======
-// router.get('/', usersControllers.getViewIndexPlus)
-//
-// router.get('/ofertas/new', usersControllers.getViewIndexPlus)
-// router.get('/ofertas/:id/edit', usersControllers.getViewIndexPlus)
-//
-// router.get('/demandas/new', usersControllers.getViewIndexPlus)
-// router.get('/demandas/:id/edit', usersControllers.getViewIndexPlus)
-//
-// router.get('/users/new', usersControllers.getViewIndexPlus)
-// router.get('/users/:id/edit', usersControllers.getViewIndexPlus)
->>>>>>> parent of 9b56467... conflict
 
 router.get('/users/all', function(req, res) {
      //return res.render('admin', {});
      return res.status(200).send({message: "pagina del admin    "});
-<<<<<<< HEAD
 
 })
 
-
-=======
-})
-
->>>>>>> parent of 9b56467... conflict
 // CRUD Ofertas ========================================================
 router.route('/ofertas/:id') // Crud a ofertas de manera individual
     .get((req, res) => {
@@ -67,15 +44,9 @@ router.route('/ofertas') // Crud a ofertas de manera grupal
 
 // CRUD Noticias =======================================================
 router.route('/noticias/:id') // Crud a noticias de manera individual
-<<<<<<< HEAD
-    .get(() => {})
-    .put(() => {})
-    .delete(() => {})
-=======
     .get((req, res) => {})
     .put((req, res) => {})
     .delete((req, res) => {})
->>>>>>> parent of 9b56467... conflict
 
 router.route('/noti') // Crud a noticias de manera grupal
     .get(function(req, res) {
@@ -83,8 +54,7 @@ router.route('/noti') // Crud a noticias de manera grupal
          return res.status(200).send({message: "pagina del admin    "});
 
     })
-<<<<<<< HEAD
-    .post(noticiasCrudController.setNewNoticia)
+    .post(newsCrudController.setNewNoticia)
 
 // CRUD Demandas =======================================================
 router.route('/demandas/:id') // Crud a demandas de manera individual
@@ -108,30 +78,7 @@ router.route('/users') // Crud a users de manera grupal
 
 //router.route('/users/all', consulta.getUsersAll)
 
-// Exportacion de las rutas
-=======
-    .post(newsCrudController.setNewNoticia)
-
-// CRUD Demandas =======================================================
-router.route('/demandas/:id') // Crud a demandas de manera individual
-    .get((req, res) => {})
-    .put((req, res) => {})
-    .delete((req, res) => {})
-
-router.route('/demandas') // Crud a demandas de manera grupal
-    .get((req, res) => {})
-    .post((req, res) => {})
 
 
-// CRUD Usuarios =======================================================
-router.route('/users/:id') // Crud a users de manera individual
-    .get((req, res) => {})
-    .put((req, res) => {})
-    .delete((req, res) => {})
-
-router.route('/users') // Crud a users de manera grupal
-    .get((req, res) => {})
-    .post((req, res) => {})
->>>>>>> parent of 9b56467... conflict
-
+    // Exportacion de las rutas
 module.exports = router;
