0d4875a0c0497a13cc216d609a10851479fa2b08
EderMarcos <skrap.marcos@gmail.com>
2017-03-13 12:19:56 -0600

    Errores con middleware error

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,18 @@
 'use strict'
 
 module.exports = (app) => {
+    // app.get('*', (req, res, next) => { // Cualquier ruta por GET que no este declarada utilizaria esta ruta
+    //     let err = new Error()
+    //     err.status = 404
+    //     next(err)
+    // })
+
+    // app.post('*', (req, res, next) => { // Cualquier ruta por POST que no este declarada utilizaria esta ruta
+    //     let err = new Error()
+    //     err.status = 404
+    //     next(err)
+    // })
+
     app.get('/', (req, res) => {  // Ruta para la pagina incial de la aplicacion
         if (req.user)
             console.log(`\n[Routes./]: Sesion actual del usuario (req.user) es :: ${JSON.stringify(req.user.id)} Nombre: ${JSON.stringify(req.user.username)} Email: ${req.user.local.email}`)
